091e04aaa0ff1a920a712d210a480ed78807ca06
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-05-17 17:42:51 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,36 @@
+#include "lib.h"
 
+bool funzione(char a)
+{
+    char a;
+    if(a>=65 && a<=90)
+    {
+        a=a+32;
+        return true;
+    }
+    else if(a>=97 && a<=122)
+    {
+        a=a-32;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+char funz1(char a)
+{
+    char a;
+    if(a>=65 && a<=90)
+    {
+        a=a+32;
+        return a;
+    }
+
+    if(a>=97 && a<=122)
+    {
+        a=a-32;
+        return a;
+    }
+}
