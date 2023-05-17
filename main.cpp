2e4c533035242e9a008548431ac8347408679404
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-05-17 17:43:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "cmake-build-debug/lib.h"
 
-int main(){
-  return 0;
+int main() {
+char a;
+bool x;
+    std::cin >> a ;
+x=funzione(a);
+if (x == false)
+{
+    std::cout << "errore"<<std::endl;
+}
+else if (x == true)
+{
+    a=funz1(a);
+    std::cout << a<< std::endl;
+}
+    return 0;
 }
